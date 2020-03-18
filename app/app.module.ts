d711158f27728e2591ed05b63bb1b6b204d16c14
Ramosjose <jramosmartinez92@gmail.com>
2020-03-18 11:01:31 -0600

    Initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { ServicePelisService } from './service-pelis.service';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    {provide: ErrorHandler, useClass: IonicErrorHandler, providers: [ServicePelisService]}
+  ]
+})
+export class AppModule {}
